8491d49d03ef9b08fb14ef643fc61d04924c7652
criss <criss.lin@hotmail.com>
2017-01-03 15:09:55 +0800

    visual v1

--- python/vis.py
+++ python/vis.py
@@ -7,15 +7,19 @@ uavs = np.array([
         [ 2. , -2.],
         [ 2. ,  2.]
     ])
-# dim * num
-sos = np.array([
-        [5., 5., 8., 8.],
-        [4., 1., 1., 4.]
-    ])
-dos = np.array([
-        [10., 10., 11., 11.],
-        [10.,  9.,  9., 10.]
-    ])
+# num * dim
+sos = [np.array([
+        [5., 4.],
+        [5., 1.],
+        [8., 1.],
+        [8., 4.]
+    ])]
+dos = [np.array([
+        [10., 10.],
+        [10., 9. ],
+        [11., 9. ],
+        [11., 10.]
+    ])]
 # num * dim
 uavsDir = np.array([
         [-2. , 12.],
@@ -23,11 +27,35 @@ uavsDir = np.array([
         [ 2. ,  8.],
         [ 2. , 12.]
     ])
+# a
+a = np.array([[          1, 2.02739e-05],
+  [          1,           0],
+  [          0,           1],
+  [         -1,          -0],
+  [         -0,          -1],
+  [   0.994616,   -0.103633],
+  [          1,           0],
+  [          0,           1],
+  [         -1,          -0],
+  [         -0,          -1]])
+# b
+b = np.array([[3.99996],
+  [  10000],
+  [  10000],
+  [  10000],
+  [  10000],
+  [3.46031],
+  [  10000],
+  [  10000],
+  [  10000],
+  [  10000]])
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 import scene_pb2
+import irispy
+import scipy
 
 fig = plt.figure(figsize=(8,8), dpi=80)
 
@@ -46,46 +74,53 @@ y_max = 14
 ax.set_xlim(x_min,x_max), ax.set_xticks(np.linspace(x_min,x_max,x_max-x_min+1,endpoint=True))
 ax.set_ylim(y_min,y_max), ax.set_yticks(np.linspace(y_min,y_max,y_max-y_min+1,endpoint=True))
 
+# uavs
 uavRadius = 1
 uavsAx = []
 for pos in uavs:
     circle = plt.Circle(pos, uavRadius, color='r')
     uavsAx.append(circle)
     ax.add_artist(circle)
-
+# uavsDir
 gDirRadius = 0.2
 uavsDirAx = []
 for pos in uavsDir:
     circle = plt.Circle(pos, gDirRadius, color='g')
     uavsDirAx.append(circle)
     ax.add_artist(circle)
-
-sos = np.array([
-        [5., 5., 8., 8.],
-        [4., 1., 1., 4.]
-    ])
-polygon = Polygon(sos.T, True, color='b')
-patchSos = ax.add_patch(polygon)
-
-dos = np.array([
-        [10., 10., 11., 11.],
-        [10.,  9.,  9., 10.]
-    ])
-polygon = Polygon(dos.T, True, color='y')
-patchDos = ax.add_patch(polygon)
+# sos
+sosAx = []
+for so in sos:
+    polygon = Polygon(so, True, color='b')    
+    patchSo = ax.add_patch(polygon)
+    sosAx.append(patchSo)
+# dos
+dosAx = []
+for do in dos:
+    polygon = Polygon(do, True, color='y')
+    patchDo = ax.add_patch(polygon)
+    dosAx.append(patchDo)
+# lcp
+lcp = irispy.Polyhedron(a, b)
+lcpPoints = lcp.getDrawingVertices()
+lcpHull = scipy.spatial.ConvexHull(lcpPoints)
+polygon = Polygon(lcpPoints[lcpHull.vertices], True, color='g', alpha=0.3)
+lcpAx = ax.add_patch(polygon)
 
 f = open('./scenes.txt', 'r')
 
 DIM = 2
     
 def update(frame):
-    global uavs, dos, sos, uavsDir, gDir
+    global uavs, dos, sos, uavsDir, gDir, a, b
 
+    # update data
     s = ''
     for line in f:
         if(line[:-1]=='###'):
             scene = scene_pb2.Scene()
             scene.ParseFromString(s[:-1])
+            #print (scene)
             #uavs
             uavsPb = scene.uavs.uav
             uavs = np.ones((len(uavsPb), DIM))
@@ -134,24 +169,30 @@ def update(frame):
             for i, dataPb in enumerate(bPb.data):
                 b[i] = dataPb
             b.resize((bPb.row, 1))
-
-
-            do = scene.dos.do[0]
-            dos = np.zeros((len(do.point), 2))
-            for i, point in enumerate(do.point):
-                dos[i,0] = point.x
-                dos[i,1] = point.y
             break;
         else:
             s += line
 
-    patchDos.set_xy(dos)
-
+    #dos
+    for i, doAx in enumerate(dosAx):
+        doAx.set_xy(dos[i])
+    #sos
+    for i, soAx in enumerate(sosAx):
+        soAx.set_xy(sos[i])
+    #uavs
     for i, uavAx in enumerate(uavsAx):
-        uavAx.center = uavs[i] + (frame % 5)
+        uavAx.center = uavs[i]
+    #uavsDir
+    for i, uavDirAx in enumerate(uavsDirAx):
+        uavDirAx.center = uavsDir[i]
+    #lcp
+    lcp = irispy.Polyhedron(a, b)
+    lcpPoints = lcp.getDrawingVertices()
+    lcpHull = scipy.spatial.ConvexHull(lcpPoints)
+    lcpAx.set_xy(lcpPoints[lcpHull.vertices])
 
     # Return the modified object
-    return patchDos, uavsAx,
+    return dosAx, sosAx, uavsAx, uavsDirAx, lcpAx
 
 import matplotlib.animation as animation
 animation = animation.FuncAnimation(fig, update, interval=100, blit=False, frames=range(10))
