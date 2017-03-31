75df7688b4a3e6257fea194eaa1895e50cf76a60
criss <criss.lin@hotmail.com>
2017-03-31 11:28:04 +0800

    vis3d_iris.py

--- python/vis3d_iris.py
+++ python/vis3d_iris.py
@@ -27,7 +27,7 @@ def plot_obstacle(point, side_length, *args, **kwargs):
     ax.add_patch(patches.Rectangle((point[0], point[1]), side_length, side_length, *args, **kwargs))
 
 
-outputFile = 'iris_res'
+outputFile = 'iris_res_3d'
 if(len(sys.argv)==2):
     outputFile = sys.argv[1]
 if os.path.isdir(outputFile):
@@ -42,7 +42,7 @@ y_min = -10
 y_max = 10
 z_min = -10
 z_max = 10
-numObs = 5
+numObs = 20
 obstacles = []
 for i in range(numObs):
     pos = np.array([
@@ -61,48 +61,38 @@ region, info = irispy.inflate_region(obstacles, seed_point, bounds, return_debug
 
 fig = plt.figure(figsize=(8,8), dpi=80)
 ax = a3.Axes3D(fig)
-ax.set_xlim(x_min, x_max)
-ax.set_ylim(y_min, y_max)
-ax.set_zlim3d(z_min, z_max)
-#ax.w_xaxis.set_pane_color((1,1,1,0))
-#ax.w_yaxis.set_pane_color((1,1,1,0))
-#ax.w_zaxis.set_pane_color((1,1,1,0))
-#ax.w_xaxis.line.set_color((1,1,1,0))
-#ax.w_yaxis.line.set_color((1,1,1,0))
-#ax.w_zaxis.line.set_color((1,1,1,0))
-#ax.set_xticks([])
-#ax.set_yticks([])
-#ax.set_zticks([])
 
-for obs in obstacles:
-    draw(obs.T, ax)
+alpha_bound = 0.05
+alpha_poly = 0.3
+alpha_ellip = 0.4
 
-info.polyhedron_history[2].draw(ax=ax)
-info.ellipsoid_history[2].draw(ax=ax)
+for i in range(2*len(info.polyhedron_history)):
+    ax.clear()
+    ax.set_xlim(ratio*x_min, ratio*x_max)
+    ax.set_ylim(ratio*y_min, ratio*y_max)
+    ax.set_zlim3d(ratio*z_min, ratio*z_max)
+    #ax.w_xaxis.set_pane_color((1,1,1,0))
+    #ax.w_yaxis.set_pane_color((1,1,1,0))
+    #ax.w_zaxis.set_pane_color((1,1,1,0))
+    ax.w_xaxis.line.set_color((1,1,1,0))
+    ax.w_yaxis.line.set_color((1,1,1,0))
+    ax.w_zaxis.line.set_color((1,1,1,0))
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_zticks([])
 
-#plt.gca().set_xlim(x_min, x_max)
-#plt.gca().set_ylim(y_min, y_max)
-#plt.gca().set_zlim3d(z_min, z_max)
-
-plt.show()
-#
-#for i in range(2*len(info.polyhedron_history)):
-#    ax.clear()
-#    ax.axis('off')
-#    ax.set_xlim([ratio*x_min, ratio*x_max])
-#    ax.set_ylim([ratio*y_min, ratio*y_max])
-#    bounds.draw(edgecolor='r')
-#    plt.scatter([0], [0], color='g')
-#    for obs in obstacles:
-#        plot_obstacle(obs.T[0], side_length, facecolor='grey', edgecolor='none')
-#    if(i%2==0):
-#        info.ellipsoid_history[i/2].draw(edgecolor='b')
-#        if(i/2>0):
-#            info.polyhedron_history[i/2-1].draw(edgecolor='g')
-#    else:
-#        info.polyhedron_history[i/2].draw(edgecolor='g')
-#        if(i/2>0):
-#            info.ellipsoid_history[i/2].draw(edgecolor='b')
-#    filename = '%06d' % i
-#    outputPath = os.path.join(outputFile, filename)
-#    plt.savefig(outputPath, dpi=fig.dpi)
+    bounds.draw(ax=ax, edgecolor='none', facecolor='r', alpha=alpha_bound)
+    ax.scatter([seed_point[0]], [seed_point[0]], [seed_point[0]], color='g')
+    for obs in obstacles:
+        draw(obs.T, ax, facecolor='black', edgecolor='none')
+    if(i%2==0):
+        info.ellipsoid_history[i/2].draw(ax=ax, facecolor='b', alpha=alpha_ellip)
+        if(i/2>0):
+            info.polyhedron_history[i/2-1].draw(ax=ax, facecolor='g', alpha=alpha_poly)
+    else:
+        info.polyhedron_history[i/2].draw(ax=ax, facecolor='g', alpha=alpha_poly)
+        if(i/2>0):
+            info.ellipsoid_history[i/2].draw(ax=ax, facecolor='b', alpha=alpha_ellip)
+    filename = '%06d' % i
+    outputPath = os.path.join(outputFile, filename)
+    plt.savefig(outputPath, dpi=fig.dpi)
